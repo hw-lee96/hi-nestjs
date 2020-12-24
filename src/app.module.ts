363dedda9fe758b3da802ec1f85203cdb46c8c5a
hw.lee <hyeongu0305@gmail.com>
2020-12-24 19:02:50 +0900

    ormconfig 설정 방식 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,32 @@ import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { UsersModule } from './users/users.module';
+import { env } from './env';
+
+const config = new env.getter().getDev();
 
 /* 데코레이터
   @Module
  - 클래스에 함수 기능을 추가
 
 */
+
 @Module({
-  imports: [MoviesModule, TypeOrmModule.forRoot({autoLoadEntities:true}), UsersModule],
-  controllers: [AppController],
-  providers: [],
+    imports: [MoviesModule, TypeOrmModule.forRoot({
+        type: 'mariadb',
+        host: config.host,
+        port: config.port,
+        username: config.username,
+        password: config.password,
+        database: config.database,
+        synchronize: true,
+        entities : config.entities,
+        autoLoadEntities: true,
+    }
+    ), UsersModule],
+    controllers: [AppController],
+    providers: [],
 })
 export class AppModule {
-  constructor(private connection: Connection) {}
+    constructor(private connection: Connection) { }
 }
